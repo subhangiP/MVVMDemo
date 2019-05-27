94e134aa3a8aeb10e09a0366820c207b2c3bc2ab
subhangi <subhangi.pawar@sts.in>
2019-05-27 15:01:28 +0530

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Subhangi Pawar on 27/05/19.
+//  Copyright © 2019 Subhangi Pawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
